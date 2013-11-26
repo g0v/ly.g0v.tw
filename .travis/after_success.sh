6657f33a8704f93186c98dc00b0851b75c899f48
Chia-liang Kao <clkao@clkao.org>
2013-11-25 17:37:38 -0800

    actually try to deploy

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 [  "$TRAVIS_PULL_REQUEST" != "false" ] || [  "$TRAVIS_BRANCH" != "master" ] && echo -e "\n" && exit 0
 
-openssl aes-256-cbc -k "$secret" -in .travis/deploy-key.enc -d -a -out .travis/deploy-key
-chmod 600 .travis/deploy-key # this key should have push access
-ssh-add .travis/deploy-key
-echo -e ">>> Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
-#./deploy
+git remote set-url origin $REPO.git
+git config --global user.email "g0v-ly@googlegroups.com"
+git config --global user.name "g0v ly (via TravisCI)"
 
+$ cat >> ~/.ssh/config <<EOF
+Host *
+IdentityFile           ~/.ssh/id_rsa
+StrictHostKeyChecking  no
+PasswordAuthentication no
+CheckHostIP            no
+BatchMode              yes
+EOF
+
+openssl aes-256-cbc -k "$secret" -in .travis/deploy-key.enc -d -a -out .ssh/id_rsa
+chmod 600 .ssh/id_rsa # this key should have push access
+echo -e ">>> Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
+./deploy
